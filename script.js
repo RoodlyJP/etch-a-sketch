c5e00052e9ece6d7184d21767e347fef1b999ef6
Roodly Jean Paul <188526128+RoodlyJP@users.noreply.github.com>
2025-12-29 01:44:33 +0000

    feat: replace hard coded value with a variable

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let container = document.querySelector("#container");
+let gridSize = 16;
 
-
-for(let i = 0; i<16; i++){
-    for(let i = 0; i<16; i++){
+for(let i = 0; i<gridSize; i++){
+    for(let i = 0; i<gridSize; i++){
         let div = document.createElement("div");
+        div.style.cssText = `width: calc(100% / ${gridSize}); height: calc(100% / ${gridSize})`;
         div.classList.add("incolor");
         div.addEventListener("mouseover", () => {
             if(div.getAttribute("class") == "incolor") {
